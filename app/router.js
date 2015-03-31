f0a4c7fef2c243dbdeab2efb1ef9f7adc678b1a9
Simon Jefford <simon.jefford@fourthhospitality.com>
2015-03-31 08:42:18 +0100

    main routes and placeholder templates

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('rotas');
+  this.route('payslips', function() {
+    this.route('payslip', {path: '/:id'});
+  });
+  this.route('rota');
+  this.route('details');
 });
 
 export default Router;
