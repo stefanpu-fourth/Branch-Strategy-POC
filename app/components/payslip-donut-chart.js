196d2061974d53107a50488530132384265b4886
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-04-02 10:31:07 +0100

    animate chart with jakob css goodness
    no need for requestAnimationFrame!

--- app/components/payslip-donut-chart.js
+++ app/components/payslip-donut-chart.js
@@ -2,5 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: [ 'payslip__pie card__flipme' ],
-  line: null
+
+  netPay: null,
+  grossPay: null,
+
+  dashOffset: 0,
+
+  takeHome: function () {
+    var props = this.getProperties('grossPay', 'netPay');
+    return (1 / props.grossPay) * props.netPay;
+  }.property('grossPay', 'netPay'),
+
+  didInsertElement: function () {
+    var takeHome = this.get('takeHome');
+
+    var el = this.element;
+    var path = el.querySelector('.payslip__chartpath');
+
+    var circumference = path.getTotalLength();
+    var dashOffset = circumference - (circumference * takeHome);
+    var strokeStyles = `stroke-dasharray:${circumference} ${circumference};stroke-dashoffset:${circumference}`;
+
+    //set the pre-animated-styles
+    path.style.cssText = strokeStyles;
+
+    Ember.run.next(() => {
+      path.style.strokeDashoffset = dashOffset;
+    });
+  }
 });
