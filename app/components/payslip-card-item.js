50d632edbd9b88cbf42ef2f0b978b21a9b8b431a
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-04-07 15:30:09 +0100

    reduce class duplication for payslip card item

--- app/components/payslip-card-item.js
+++ app/components/payslip-card-item.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import CardItemMixin from 'ess/mixins/card-item';
 
 export default Ember.Component.extend(CardItemMixin, {
-  classNames: ['card', 'payslip']
+  classNames: ['payslip']
 });
