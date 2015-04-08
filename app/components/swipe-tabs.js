a45aa599a13694c173070dcbcb36d173c186e730
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-04-08 13:57:19 +0100

    set scroll left on didInsertElement

--- app/components/swipe-tabs.js
+++ app/components/swipe-tabs.js
@@ -1,4 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['swipe__tabs'],
+
+  selectedIndex: 10,
+
+  didInsertElement: function () {
+    var selectedIndex = this.get('selectedIndex');
+    var $tabs = this.$('.swipe__tablink');
+    var $active = $tabs.eq(selectedIndex);
+    var viewPortWidth = this.getViewPortWidth();
+    var activeElWidth = $active.outerWidth();
+    var totalTabWidth = $tabs.length * activeElWidth;
+    var $container;
+    var left;
+    var scrollLeft;
+    var adjustedWidth;
+
+    $tabs.removeClass('swipe__tablink--active');
+    $active.addClass('swipe__tablink--active');
+
+    if ((viewPortWidth + 12) > totalTabWidth) {
+      return;
+    }
+
+    $container = this.$();
+    left = $active.position().left;
+    scrollLeft = $container.scrollLeft();
+    adjustedWidth = (viewPortWidth / 2) - (activeElWidth / 2);
+
+    $container.animate(scrollLeft + left - adjustedWidth);
+  },
+
+  setScrollLeft: function () {
+
+  },
+
+  getViewPortWidth: function() {
+    return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  },
 });
