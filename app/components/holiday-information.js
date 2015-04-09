e346ef16447b789818ade6fbdbe7d3712f840ac6
santiagoGuti <Santiago.Montes@fourth.com>
2015-04-09 17:03:30 +0100

    Add holiday reserved

--- app/components/holiday-information.js
+++ app/components/holiday-information.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: 'rota__holiday'
+  classNames: 'rota__holiday',
+
+  displayReservedHoliday: Ember.computed.gte('info.reservedHoliday', 1)
 });
--- app/models/holidaybalance.js
+++ app/models/holidaybalance.js
@@ -7,10 +7,11 @@ var HolidayBalance = DS.Model.extend({
   bookedHoliday: attr('number'),
   takenHoliday: attr('number'),
   accruedHoliday: attr('number'),
+  reservedHoliday: attr('number'),
   periodHoliday: attr('string'),
 
   remainingHoliday: function () {
-    return this.get('allowedHoliday')-this.get('takenHoliday');
+    return this.get('allowedHoliday')-(this.get('takenHoliday')+this.get('bookedHoliday'));
   }.property('allowedHoliday','takenHoliday')
 });
 
@@ -20,6 +21,7 @@ HolidayBalance.reopenClass({
     allowedHoliday: 20,
     bookedHoliday: 3,
     takenHoliday: 4,
+    reservedHoliday:1,
     accruedHoliday: 5,
     periodHoliday:"(01 Jan - 31 Dec 2015)"
   }]
