6a5b22075fdc4559c9eda663f5577015aea72f9c
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-04-09 14:55:09 +0100

    missing translations for payslips

--- app/translations.js
+++ app/translations.js
@@ -4,6 +4,12 @@ export default {
       rotas: 'My Rotas',
       payslips: 'My Payslips',
       details: 'HR Details'
+    },
+    payslips: {
+      breakdown: 'Show payslip breakdown',
+      download: 'Download payslip (.pdf)',
+      totalPay: 'Total',
+      takeHome: 'Take home'
     }
   }
 };
