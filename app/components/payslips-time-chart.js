e9d6acc39d2572ae5eac5b964050ae98168ae325
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-04-10 11:44:55 +0100

    translations and formatting

--- app/components/payslips-time-chart.js
+++ app/components/payslips-time-chart.js
@@ -1,32 +1,56 @@
 import Ember from 'ember';
+import i18n from 'fourth-components/i18n';
 
 export default Ember.Component.extend({
   classNames: ['payslip__line'],
 
   payslips: null,
 
+  grossPayTitle: i18n.t('payslips.totalPay'),
+
+  netPayTitle: i18n.t('payslips.takeHome'),
+
   xCategories: function () {
     return this.get('payslips').getEach('formattedProcessingDate').toArray();
-  }.property('payslip'),
+  }.property('payslips'),
 
   grossPaySeries: function () {
     return this.get('payslips').getEach('grossPay').toArray();
-  }.property('payslip'),
+  }.property('payslips'),
 
   netPaySeries: function () {
     return this.get('payslips').getEach('netPay').toArray();
-  }.property('payslip'),
+  }.property('payslips'),
+
+  labelFormatter: function () {
+    return `£${this.value}`.htmlSafe();
+  },
+
+  tooltipFormatter: function () {
+    var title = this.x;
+    var seriesMarkup;
+
+    seriesMarkup = this.points.map(item => {
+      var name = item.series.name;
+      var pay = parseFloat(item.y).toFixed(2);
+
+      return `${name}:£${pay}`;
+    }).join('<br />');
+
+    return `<strong>${title}</strong><br />${seriesMarkup}`.htmlSafe();
+  },
 
   didInsertElement: function() {
-    var props = this.getProperties('xCategories', 'grossPaySeries', 'netPaySeries');
+    var propNames = [ 'xCategories', 'grossPaySeries', 'netPaySeries', 'grossPayTitle', 'netPayTitle' ];
+    var props = this.getProperties(propNames);
 
     this.$().highcharts({
+      title: null,
       chart: {
         type: 'spline',
         marginLeft: 0,
         marginRight: 0
       },
-      title: null,
       xAxis: {
         categories: props.xCategories,
         tickmarkPlacement: 'on',
@@ -57,26 +81,25 @@ export default Ember.Component.extend({
           style: {
             'color': '#999999'
           },
-          formatter: function () {
-            return '£' + this.value;
-          }
+          formatter: this.labelFormatter
         }
       },
       tooltip: {
+        formatter: this.tooltipFormatter,
         shared: true
       },
       series: [{
         marker: {
           symbol: 'circle'
         },
-        name: 'Total',
+        name: props.grossPayTitle,
         color: '#CCC',
         data: props.grossPaySeries
       }, {
         marker: {
           symbol: 'circle'
         },
-        name: 'Take home',
+        name: props.netPayTitle,
         color: '#9cbf2b',
         data: props.netPaySeries
       }]
