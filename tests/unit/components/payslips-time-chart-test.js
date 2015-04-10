8797e454a60c55f3e0f82f6510a85503e2e32d21
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-04-10 13:05:32 +0100

    pass mock payslips data to payslips-chart test

--- tests/unit/components/payslips-time-chart-test.js
+++ tests/unit/components/payslips-time-chart-test.js
@@ -2,6 +2,7 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForComponent('payslips-time-chart', {
   // Specify the other units that are required for this test
@@ -12,7 +13,13 @@ test('it renders', function(assert) {
   assert.expect(2);
 
   // Creates the component instance
-  var component = this.subject();
+  var component = this.subject({
+    payslips: [
+      Ember.Object.create({ grossPay: 200, netPay: 180, formattedProcessingDate: '1 Mar' }),
+      Ember.Object.create({ grossPay: 180, netPay: 160, formattedProcessingDate: '1 Apr' })
+    ]
+  });
+
   assert.equal(component._state, 'preRender');
 
   // Renders the component to the page
