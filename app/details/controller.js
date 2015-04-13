c214393c0b309c479dbbad682511110f886e6953
santiagoGuti <Santiago.Montes@fourth.com>
2015-04-13 17:28:35 +0100

    use of empty function instead of blank

--- app/details/controller.js
+++ app/details/controller.js
@@ -3,20 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   attrs: {},
 
-  isBlankAddress2: function(){
-    return Ember.isBlank(this.get('attrs.employee.address2'));
-  }.property('attrs.employee.address2'),
-
-  isBlankAddress3: function(){
-    return Ember.isBlank(this.get('attrs.employee.address3'));
-  }.property('attrs.employee.address3'),
-
-  isBlankCounty: function(){
-    return Ember.isBlank(this.get('attrs.employee.county'));
-  }.property('attrs.employee.county'),
-
-  isBlankCountry: function(){
-    return Ember.isBlank(this.get('attrs.employee.country'));
-  }.property('attrs.employee.country')
+  isBlankAddress2: Ember.computed.empty('attrs.employee.address2'),
+  isBlankAddress3: Ember.computed.empty('attrs.employee.address3'),
+  isBlankCounty: Ember.computed.empty('attrs.employee.county'),
+  isBlankCountry: Ember.computed.empty('attrs.employee.country')
 
 });
