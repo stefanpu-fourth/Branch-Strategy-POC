142ac05022ae84617ec303cab33e5fa811742a6f
santiagoGuti <Santiago.Montes@fourth.com>
2015-04-13 14:50:55 +0100

    bug don't display blank fields address (in no mandatory only)

--- app/details/controller.js
+++ app/details/controller.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  attrs: {}
+  attrs: {},
+
+  isBlankAddress2: function(){
+    return Ember.isBlank(this.get('attrs.employee.address2'));
+  }.property('attrs.employee.address2'),
+
+  isBlankAddress3: function(){
+    return Ember.isBlank(this.get('attrs.employee.address3'));
+  }.property('attrs.employee.address3'),
+
+  isBlankCounty: function(){
+    return Ember.isBlank(this.get('attrs.employee.county'));
+  }.property('attrs.employee.county'),
+
+  isBlankCountry: function(){
+    return Ember.isBlank(this.get('attrs.employee.country'));
+  }.property('attrs.employee.country')
+
 });
