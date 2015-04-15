2a7e3761b5bd153bc67fd26f5a2c3af5d2414d2f
Steve Sims <steve.sims@fourth.com>
2015-04-15 12:02:20 +0100

    First version of rota bar display
    NB this version doesn't yet deal with shifts that overflow to the following day

---
+++ app/components/rota-bar-segment/component.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'span',
+
+  classNames: ['rota__shift'],
+
+  attributeBindings: ['style'],
+
+  style: function() {
+    var shift = this.get('shift');
+    return "left: " + this.getStartPercent(shift) + "%; width: " + this.getDurationPercent(shift) + "%";
+  }.property('shift'),
+
+  getStartPercent: function(shift) {
+    var start = this.getIntTime(shift.start);
+
+    return (start/(60*24)) * 100;
+  },
+
+  getIntTime: function(time) {
+    var hour = parseInt(time.substring(0,2));
+    var minutes = parseInt(time.substring(2,4));
+    return (hour * 60) + minutes;
+  },
+
+  getDurationPercent: function(shift) {
+    var start = this.getIntTime(shift.start);
+    var end = this.getIntTime(shift.end);
+    if (end < start) {
+      end = end + this.getIntTime('2400');
+    }
+
+    return ((end - start)/(60*24)) * 100;
+  }
+});
