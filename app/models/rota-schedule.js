ebaf936ed7b2f69fef5526003cdef9985109e554
Steve Sims <steve.sims@fourth.com>
2015-04-16 16:32:18 +0100

    Added in some extra rota schedule data

--- app/models/rota-schedule.js
+++ app/models/rota-schedule.js
@@ -70,7 +70,7 @@ var fixtures = [{
 }, {
   id: 12,
   type: 'on',
-  shiftTimes: ['2200', '0100'],
+  shiftTimes: ['2200', '0100', '0000', '0000'],
   isMain: true
 }, {
   id: 13,
@@ -84,7 +84,7 @@ var fixtures = [{
 }, {
   id: 15,
   type: 'on',
-  shiftTimes: ['0700', '1500', '2100', '0100'],
+  shiftTimes: ['2100', '0100'],
   isMain: true
 }, {
   id: 16,
@@ -112,22 +112,108 @@ var fixtures = [{
   id: 21,
   type: 'off',
   isMain: true
+}, {
+  id: 22,
+  type: 'on',
+  shiftTimes: ['2200', '0100', '0000', '0000'],
+  isMain: true
+}, {
+  id: 23,
+  type: 'on',
+  shiftTimes: ['2200', '0100'],
+  isMain: true
+}, {
+  id: 24,
+  type: 'Holiday',
+  isMain: true
+}, {
+  id: 25,
+  type: 'on',
+  shiftTimes: ['0700', '1500', '2100', '0100'],
+  isMain: true
+}, {
+  id: 26,
+  type: 'off',
+  isMain: true
+}, {
+  id: 27,
+  type: 'off',
+  isMain: true
+}, {
+  id: 28,
+  type: 'off',
+  isMain: true
+}, {
+  id: 29,
+  type: 'on',
+  shiftTimes: ['0700', '1500', '2100', '0100'],
+  isMain: true
+}, {
+  id: 30,
+  type: 'on',
+  shiftTimes: ['0700', '0100'],
+  isMain: true
+}, {
+  id: 31,
+  type: 'off',
+  isMain: true
+}, {
+  id: 32,
+  type: 'on',
+  shiftTimes: ['2200', '0100', '0000', '0000'],
+  isMain: true
+}, {
+  id: 33,
+  type: 'on',
+  shiftTimes: ['2200', '0100'],
+  isMain: true
+}, {
+  id: 34,
+  type: 'Holiday',
+  isMain: true
+}, {
+  id: 35,
+  type: 'on',
+  shiftTimes: ['0700', '1500', '2100', '0100'],
+  isMain: true
+}, {
+  id: 36,
+  type: 'off',
+  isMain: true
 }];
 
-var shiftMoment = moment().startOf('isoWeek').subtract(7, 'days');
+fixtures.forEach(f => {
+  f.location = "Here",
+  f.jobTitle = "Surf"
+});
+
+var shiftMoment = moment().startOf('isoWeek').subtract(14, 'days');
 fixtures.forEach(f => {
   f.shiftDate = shiftMoment.format("YYYY-MM-DD 00:00:00.000");
   shiftMoment.add(1, 'day');
 });
 
+shiftMoment = moment().startOf('isoWeek');
+fixtures.push({
+  id: 37,
+  type: 'on',
+  shiftDate: shiftMoment.format("YYYY-MM-DD 00:00:00.000"),
+  shiftTimes: ['0700', '1500'],
+  location: "There",
+  jobTitle: "Slave"
+});
+
 fixtures.forEach(f => {
   if (f.shiftTimes) {
-    let newShifts = f.shiftTimes.map(function(time, index) {
+    let newShifts = f.shiftTimes.map(function(startTime, index) {
       if ((index % 2) === 0) {
-        return {
-          start: time,
-          end: f.shiftTimes[index + 1]
-        };
+        var endTime = f.shiftTimes[index + 1];
+        if (startTime !== endTime) {
+          return {
+            start: startTime,
+            end: endTime
+          };
+        }
       }
     });
     f.shifts = newShifts.filter(shift => { return shift !== undefined; });
