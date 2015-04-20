4fcbdff6daaeb466f84015daf578a06c6fa29ef0
santiagoGuti <Santiago.Montes@fourth.com>
2015-04-20 10:16:52 +0100

    take out unnecessary code

--- app/details/controller.js
+++ app/details/controller.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  attrs: {},
-
-  isBlankAddress2: Ember.computed.empty('attrs.employee.address2'),
-  isBlankAddress3: Ember.computed.empty('attrs.employee.address3'),
-  isBlankCounty: Ember.computed.empty('attrs.employee.county'),
-  isBlankCountry: Ember.computed.empty('attrs.employee.country')
-
+  attrs: {}
 });
