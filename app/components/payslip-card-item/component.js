2b9987b52d3ed8d468d2ea926fd2dd48b9ac283b
Simon Jefford <simon.jefford@fourth.com>
2015-04-22 13:43:18 +0100

    format the processing date above the donut

--- app/components/payslip-card-item/component.js
+++ app/components/payslip-card-item/component.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 import CardItemMixin from 'ess/mixins/card-item';
 
 export default Ember.Component.extend(CardItemMixin, {
-  classNames: ['payslip']
+  classNames: ['payslip'],
+
+  formattedProcessingDate: function() {
+    return moment(this.get('payslip.processingDate')).format('ddd Do MMM');
+  }.property('payslip.processingDate')
 });
