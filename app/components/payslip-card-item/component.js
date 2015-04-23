58fe75e78b4b08b1ff757829a4cf0ad723419fe1
Simon Jefford <simon.jefford@fourth.com>
2015-04-23 11:27:34 +0100

    add the year to the date on the payslip card

--- app/components/payslip-card-item/component.js
+++ app/components/payslip-card-item/component.js
@@ -5,6 +5,6 @@ export default Ember.Component.extend(CardItemMixin, {
   classNames: ['payslip'],
 
   formattedProcessingDate: function() {
-    return moment(this.get('payslip.processingDate')).format('ddd Do MMM');
+    return moment(this.get('payslip.processingDate')).format('ddd Do MMM YYYY');
   }.property('payslip.processingDate')
 });
