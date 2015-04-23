1b3d1b01d7429834aac917a210b89273149717db
Simon Jefford <simon.jefford@fourth.com>
2015-04-23 10:58:21 +0100

    we get an id now so we don't need this

--- app/serializers/payslip.js
+++
@@ -1,8 +0,0 @@
-import Siren from './siren';
-
-export default Siren.extend({
-  normalize: function(type, hash, prop) {
-    hash.id = hash.processingDate;
-    return this._super(type, hash, prop);
-  }
-});
