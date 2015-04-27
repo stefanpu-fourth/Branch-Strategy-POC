75137c049372dd05e913f73aba139a769e7cfbdb
Simon Jefford <simon.jefford@fourth.com>
2015-04-27 15:28:00 +0100

    payslips.index doesn't exist any more

--- app/index/route.js
+++ app/index/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    return this.transitionTo('payslips.index');
+    return this.transitionTo('payslips');
   }
 });
