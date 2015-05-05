cbecce5f1423c324ec710251153dd7461917e535
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-05-05 11:37:27 +0100

    tidying up setSelectedIndex action

--- app/payslips/route.js
+++ app/payslips/route.js
@@ -24,15 +24,21 @@ export default Ember.Route.extend({
     setSelectedIndex: function (index) {
       var attrs = this.get('controller.attrs');
       var currentIndex = attrs.selectedIndex;
+      var hasCurrentIndex = typeof currentIndex !== 'undefined' || currentIndex !== null;
 
+      //set the value of index to be index or the length of the collection - 1
       index = typeof index !== 'undefined' && index !== null ? index : attrs.payslips.get('content.length') - 1;
 
-      if ((typeof currentIndex !== 'undefined' || currentIndex !== null) && index === currentIndex) {
+      //don't set the index if it hasn't changed
+      if (hasCurrentIndex && index === currentIndex) {
         return;
       }
 
+      //set selectedIndex on controller attrs hash
+      //set is panning only if we don't have a currentIndex as this
+      //creates weird view behaviour (panning when we don't want to)
       this.set('controller.attrs.selectedIndex', index);
-      this.set('controller.attrs.isPanning', typeof currentIndex === 'undefined' || currentIndex === null);
+      this.set('controller.attrs.isPanning', !hasCurrentIndex);
     }
   }
 
