988cdbf2daf515ad59ee7904ae3521dc5b23ffcd
Simon Jefford <simon.jefford@fourth.com>
2015-05-05 11:04:57 +0100

    forward flipCard from front & back components

--- app/components/flip-card-back/component.js
+++ app/components/flip-card-back/component.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['flip-card--back']
+  classNames: ['flip-card--back'],
+
+  actions: {
+    flipCard: function() {
+      this.get('parentView').send('flipCard');
+    }
+  }
 });
--- app/components/flip-card-front/component.js
+++ app/components/flip-card-front/component.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['flip-card--front']
+  classNames: ['flip-card--front'],
+
+  actions: {
+    flipCard: function() {
+      this.get('parentView').send('flipCard');
+    }
+  }
 });
