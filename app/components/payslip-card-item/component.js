b39ba12e85c43992bd862d87cbc2b815b264a22e
santiagoGuti <Santiago.Montes@fourth.com>
2015-05-06 10:26:06 +0100

    Use helper instead of create a function in payslip date

--- app/components/payslip-card-item/component.js
+++ app/components/payslip-card-item/component.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 import CardItemMixin from 'ess/mixins/card-item';
 
 export default Ember.Component.extend(CardItemMixin, {
-  classNames: ['payslip'],
-
-  formattedProcessingDate: function() {
-    return moment(this.get('payslip.processingDate')).format('DD MMM YYYY');
-  }.property('payslip.processingDate')
+  classNames: ['payslip']
 });
