2270e77bc33c73ccc80534383607d1bcdecd2f2c
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-05-06 17:33:13 +0100

    controller-pageable mixin

--- app/payslips/controller.js
+++ app/payslips/controller.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+import Pageable from 'ess/mixins/controller-pageable';
 
 var PayslipContainer = Ember.ArrayProxy.extend(Ember.SortableMixin, {
   sortProperties: ['processingDate']
 });
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(Pageable, {
   attrs: {
     payslips: PayslipContainer.create()
   }
