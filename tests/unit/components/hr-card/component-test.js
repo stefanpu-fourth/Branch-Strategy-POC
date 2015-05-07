fd7e5d0ab67ffcaa51c1dceb837695110cd5edb4
Jakob <jakob.buhl@fourth.com>
2015-05-07 17:57:31 +0100

    d'Oh! Forgot to register t-helper

--- tests/unit/components/hr-card/component-test.js
+++ tests/unit/components/hr-card/component-test.js
@@ -2,10 +2,14 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
+import t from 'ess/helpers/t';
 
 moduleForComponent('hr-card', {
-  // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:svg-icon'],
+  beforeEach: function() {
+    Ember.Handlebars.registerHelper('t', t);
+  }
 });
 
 test('it renders', function(assert) {
