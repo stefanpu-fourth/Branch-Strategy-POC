eba4048b429fe3328628c7df6ca955729bc379c1
santiagoGuti <Santiago.Montes@fourth.com>
2015-05-07 11:52:22 +0100

    white shift for (none shift) now animated, need review for jakob?

--- app/components/rota-bar/component.js
+++ app/components/rota-bar/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['rota-bar'],
-  classNameBindings: ['empty:-empty', 'isInPast:-past'],
+  classNameBindings: ['isInPast:-past'],
 
   shifts: null,
   empty: null,
