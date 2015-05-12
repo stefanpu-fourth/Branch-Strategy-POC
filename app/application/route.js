00d0448cceb6efadb1ebf53bf7b37721c4bd8f73
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-05-12 11:39:57 +0100

    reload current route on employee change

--- app/application/route.js
+++ app/application/route.js
@@ -11,10 +11,17 @@ export default Ember.Route.extend({
     this.set('appStateService.rootResource', rootResource.get('firstObject'));
   },
 
+  refreshCurrentRoute: function () {
+    var handlerInfos = this.get('router.router.currentHandlerInfos');
+    var handler = handlerInfos[handlerInfos.length - 1];
+    this.container.lookup(`route:${handler.name}`).refresh();
+  },
+
   actions: {
     setCurrentEmployee: function (employeeId) {
       var employee = this.store.getById('root', employeeId) || this.store.createRecord('root', { id: employeeId });
       this.set('appStateService.rootResource', employee);
+      this.refreshCurrentRoute();
     }
   }
 });
