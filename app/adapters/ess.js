19f7c3f1cd6792f017e5f4130c12878fd1eb4206
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-05-12 10:03:11 +0100

    headers and namespace to change on employee id set

--- app/adapters/ess.js
+++ app/adapters/ess.js
@@ -1,24 +1,27 @@
 import SirenAdapter from './siren';
-import config from 'ess/config/environment';
 import Ember from 'ember';
+import config from 'ess/config/environment';
 
 export default SirenAdapter.extend({
   appStateService: Ember.inject.service(),
 
+  employeeId: Ember.computed.alias('appStateService.authenticatedEmployeeId'),
+
   headers: function() {
-    var headers = {
+    var employeeId = this.get('employeeId');
+
+    return (employeeId && employeeId !== '0') ? {
       "X-Fourth-Version": 0,
-      "X-Fourth-UserID": config.user,
+      "X-Fourth-UserID": employeeId,
       "X-Fourth-Org": 0
-    };
-    return (config.user && config.user !== '0') ? headers : {};
-  }.property(),
+    } : {};
+  }.property('employeeId'),
 
   host: function() {
     return config.apiBaseUrl;
   }.property(),
 
   namespace: function() {
-    return `employees/${this.get('appStateService.authenticatedEmployeeId')}`;
-  }.property()
+    return `employees/${this.get('employeeId')}`;
+  }.property('employeeId')
 });
