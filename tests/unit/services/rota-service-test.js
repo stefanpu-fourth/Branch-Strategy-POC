e32aa14455d5cdba58aafef6ae5e9a0b3f92d83d
avcengineering <Andrew.vanCampenhout@fourth.com>
2015-05-15 16:03:22 +0100

    fix for rota-service tests

--- tests/unit/services/rota-service-test.js
+++ tests/unit/services/rota-service-test.js
@@ -1,9 +1,11 @@
 import {
   moduleFor,
   test
-} from 'ember-qunit';
+}
+from 'ember-qunit';
 
 import Ember from 'ember';
+import DS from 'ember-data';
 import RotaSchedule from 'ess/models/rota-schedule';
 
 var store = {};
@@ -12,7 +14,11 @@ var records;
 moduleFor('service:rota-service', {
   beforeEach: function() {
     var container = this.container;
-    DS._setupContainer(container);
+
+    if (!container.lookup('store:main')) {
+      DS._setupContainer(container);
+    }
+
     store = container.lookup('store:main');
     container.register('model:rota-schedule', RotaSchedule);
     records = [];
@@ -20,7 +26,7 @@ moduleFor('service:rota-service', {
 
     var m = moment(new Date(2015, 2, 30));
 
-    for (let i=0;i<35;i++) {
+    for (let i = 0; i < 35; i++) {
       records.push(store.createRecord('rota-schedule', {
         shiftDate: m.format('YYYY-MM-DD')
       }));
@@ -33,34 +39,21 @@ moduleFor('service:rota-service', {
   }
 });
 
-test('it fetches the appropriate data from the store', function(assert) {
-  var service = this.subject({ store: store });
-  assert.ok(service);
-  service.getRotaWeeks(new Date(2015, 3, 2));
-  sinon.assert.calledWith(store.find, 'rota-schedule', {
-    RequestDate: '2015-04-02', // obviously months are zero based when calling Date.now()...
-    NoPreviousWeeks: 2,
-    NoFutureWeeks: 2
-  });
-});
-
 test('it groups the rotaSchedule objects into rota weeks', function(assert) {
-  var service = this.subject({ store: store });
-  var weeksPromise = service.getRotaWeeks(new Date(2015, 3, 2));
+  var service = this.subject();
+  var weeks = service.getRotaWeeks(records, new Date(2015, 3, 2));
 
-  return weeksPromise.then(weeks => {
-    assert.equal(weeks.get('firstObject.start').format('YYYY-MM-DD'), '2015-03-30', 'first shift is present');
-    assert.equal(weeks.get('lastObject.start').format('YYYY-MM-DD'), '2015-04-27', 'last shift is present');
-    assert.equal(weeks.get('length'), 5, 'expected number of rota weeks loaded');
-    weeks.forEach(week => {
-      var formattedShiftStart = week.get('start').format('YYYY-MM-DD');
-      assert.equal(week.get('shifts.length'), 7, `rota week starting ${formattedShiftStart} is complete`);
-    });
+  assert.equal(weeks.get('firstObject.start').format('YYYY-MM-DD'), '2015-03-30', 'first shift is present');
+  assert.equal(weeks.get('lastObject.start').format('YYYY-MM-DD'), '2015-04-27', 'last shift is present');
+  assert.equal(weeks.get('length'), 5, 'expected number of rota weeks loaded');
+  weeks.forEach(week => {
+    var formattedShiftStart = week.get('start').format('YYYY-MM-DD');
+    assert.equal(week.get('shifts.length'), 7, `rota week starting ${formattedShiftStart} is complete`);
   });
 });
 
 test('it consolidates days in rota weeks', function(assert) {
-  var service = this.subject({ store: store });
+  var service = this.subject();
 
   Ember.run(() => {
     // first up add in a shift on a day
@@ -89,29 +82,29 @@ test('it consolidates days in rota weeks', function(assert) {
       shiftTimes: ['1600', '1700']
     }));
 
-    var weeksPromise = service.getRotaWeeks(new Date(2015, 3, 2));
-
-    weeksPromise.then(weeks => {
-      assert.equal(weeks.get('firstObject.start').format('YYYY-MM-DD'), '2015-03-30', 'first shift is present');
-      assert.equal(weeks.get('lastObject.start').format('YYYY-MM-DD'), '2015-04-27', 'last shift is present');
-      assert.equal(weeks.get('length'), 5, 'expected number of rota weeks loaded');
-      weeks.forEach(week => {
-        var formattedShiftStart = week.get('start').format('YYYY-MM-DD');
-        assert.equal(week.get('shifts.length'), 7, `rota week starting ${formattedShiftStart} is complete`);
-      });
-
-      assert.equal(record.shifts.length, 2, 'shifts get merged as expected');
-
-      weeksPromise = service.getRotaWeeks(new Date(2015, 3, 2));
-      weeksPromise.then(weeks => {
-        assert.equal(record.shifts.length, 2, 'shifts merged as expected after second call to getRotaWeeks');
-      });
+    records.forEach(day => {
+      day.calculateShifts();
     });
+
+    var weeks = service.getRotaWeeks(records, new Date(2015, 3, 2));
+
+    assert.equal(weeks.get('firstObject.start').format('YYYY-MM-DD'), '2015-03-30', 'first shift is present');
+    assert.equal(weeks.get('lastObject.start').format('YYYY-MM-DD'), '2015-04-27', 'last shift is present');
+    assert.equal(weeks.get('length'), 5, 'expected number of rota weeks loaded');
+    weeks.forEach(week => {
+      var formattedShiftStart = week.get('start').format('YYYY-MM-DD');
+      assert.equal(week.get('shifts.length'), 7, `rota week starting ${formattedShiftStart} is complete`);
+    });
+
+    assert.equal(record.shifts.length, 2, 'shifts get merged as expected');
+
+    weeks = service.getRotaWeeks(records, new Date(2015, 3, 2));
+    assert.equal(record.shifts.length, 2, 'shifts merged as expected after second call to getRotaWeeks');
   });
 });
 
 test('it can get the next shift when there is one', function(assert) {
-  var service = this.subject({ store: store });
+  var service = this.subject();
 
   // add a shift to a record in the future
   var record = records.get(20);
@@ -121,27 +114,31 @@ test('it can get the next shift when there is one', function(assert) {
   record = records.get(1);
   record.shiftTimes = ['0600', '1200'];
 
-  var shiftPromise = service.getNextShift(new Date(2015, 3, 2));
-
-  return shiftPromise.then(fetchedShift => {
-    // fetched shift should be the future one
-    assert.equal(fetchedShift.start, '0700');
-    assert.equal(fetchedShift.end, '1300');
+  records.forEach(day => {
+    day.calculateShifts();
   });
+
+  var fetchedShift = service.getNextShift(records, new Date(2015, 3, 2));
+
+  // fetched shift should be the future one
+  assert.equal(fetchedShift.start, '0700');
+  assert.equal(fetchedShift.end, '1300');
+
 });
 
 test("won't return a next shift when there isn't one", function(assert) {
-  var service = this.subject({ store: store });
-
-  var shiftPromise = service.getNextShift(new Date(2015, 3, 2));
-
-  return shiftPromise.then(fetchedShift => {
-    assert.equal(fetchedShift, undefined);
+  var service = this.subject({
+    store: store
   });
+
+  var fetchedShift = service.getNextShift(records, new Date(2015, 3, 2));
+  assert.equal(fetchedShift, undefined);
 });
 
 test("merged schedules correctly sort their shifts", function(assert) {
-  var service = this.subject({ store: store });
+  var service = this.subject({
+    store: store
+  });
 
   // add a shift to a record late in the day
   var record = records.get(20);
@@ -161,11 +158,15 @@ test("merged schedules correctly sort their shifts", function(assert) {
     // and add a shift to a record in the past
     record = records.get(1);
     record.shiftTimes = ['0600', '1200'];
-    var shiftPromise = service.getNextShift(shiftDate);
-    shiftPromise.then(fetchedShift => {
-      // fetched shift should be one from the merged-in record
-      assert.equal(fetchedShift.start, '0700');
-      assert.equal(fetchedShift.end, '1200');
+
+    records.forEach(day => {
+      day.calculateShifts();
     });
+
+    var fetchedShift = service.getNextShift(records, shiftDate);
+
+    // fetched shift should be one from the merged-in record
+    assert.equal(fetchedShift.start, '0700');
+    assert.equal(fetchedShift.end, '1200');
   });
 });
