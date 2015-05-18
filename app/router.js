4272ec795666ec7882a3c54ddabbf7642c112e77
santiagoGuti <Santiago.Montes@fourth.com>
2015-05-18 11:55:31 +0100

    Better structure of the code for get title, get rid of the timer

--- app/router.js
+++ app/router.js
@@ -1,18 +1,28 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+//var get = Ember.get;
+
 var Router = Ember.Router.extend({
   location: config.locationType,
 
-  notifyGoogleAnalytics: function() {
-    var url = this.get('url');
+  application: function () {
+    return this.container.lookup('controller:application');
+  }.property(),
 
-    window.setTimeout(function() {
-      ga('send', 'pageview', {
-        'page': url,
-        'title': document.title
-      });
-    }, 1);
+  getCurrentTitle: function() {
+    var title = this.get('application.currentRouteName');
+    return this.container.lookup(`route:${title}`).get('title');
+  }.property('application.currentRouteName'),
+
+  notifyGoogleAnalytics: function() {
+    var props = this.getProperties('url', 'getCurrentTitle');
+
+    ga('send', 'pageview', {
+      'page': props.url,
+      'title': props.getCurrentTitle,
+      'location': config.locationType
+    });
 
     return true;
   }.on('didTransition')
