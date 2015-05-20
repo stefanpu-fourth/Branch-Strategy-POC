62462ec81154605aefa9ebdd886463f51b82feb2
avcengineering <Andrew.vanCampenhout@fourth.com>
2015-05-20 17:18:05 +0100

    odata pageable test coverage

--- tests/unit/mixins/adapter-pageable-test.js
+++ tests/unit/mixins/adapter-pageable-test.js
@@ -1,12 +1,31 @@
 import Ember from 'ember';
+import ODataAdapter from '../../../adapters/odata';
 import AdapterPageableMixin from '../../../mixins/adapter-pageable';
-import { module, test } from 'qunit';
+import {
+  module, test
+}
+from 'qunit';
 
 module('AdapterPageableMixin');
 
+var AdapterPageableObject = ODataAdapter.extend(AdapterPageableMixin, {
+  host: 'http://localhost:4200',
+  namespace: ''
+});
+var adapter = AdapterPageableObject.create();
+
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var AdapterPageableObject = Ember.Object.extend(AdapterPageableMixin);
-  var subject = AdapterPageableObject.create();
-  assert.ok(subject);
+  assert.ok(adapter);
+});
+
+test('getQueryStringParts returns an array populated by the pageable mixin', function(assert) {
+  var result = adapter.getQueryStringParts({
+    typeKey: 'hamster'
+  }, {
+    items: 10,
+    page: 1
+  }, []);
+
+  assert.deepEqual(result, ['$skip=0&$top=10'], 'getQueryStringParts for sortable returns an array with a single entry for $skip and $top');
 });
