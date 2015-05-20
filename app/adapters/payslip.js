13c2d9a784f9b05b5ea1819dd6ca2abd93e650b8
avcengineering <Andrew.vanCampenhout@fourth.com>
2015-05-20 11:28:00 +0100

    remove getODataUrlParts override from payslips

--- app/adapters/payslip.js
+++ app/adapters/payslip.js
@@ -3,24 +3,4 @@ import Sortable from 'ess/mixins/adapter-sortable';
 import Filterable from 'ess/mixins/adapter-filterable';
 import Pageable from 'ess/mixins/adapter-pageable';
 
-export default ODataAdapter.extend(Sortable, Filterable, Pageable, {
-  getODataUrlParts(type, query) {
-    var {
-      sort, filters, items, page
-    } = query;
-
-    delete query.sort;
-    delete query.filters;
-    delete query.items;
-    delete query.page;
-
-    return [
-      this.urlPrefix(),
-      this.pathForType(type.typeKey),
-      '?',
-      this.getFiltersString(filters),
-      this.getSortString(sort.by, sort.dir),
-      this.getPaginationString(items, page)
-    ];
-  }
-});
+export default ODataAdapter.extend(Sortable, Filterable, Pageable, {});
