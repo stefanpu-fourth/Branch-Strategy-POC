25306dba61b50fc726571c533a6df05ddf3df552
Steve Sims <steve.sims@fourth.com>
2015-06-02 10:19:47 +0100

    Adjusted rota-schedule display day to be midnight to midnight

--- app/serializers/rota-schedule.js
+++ app/serializers/rota-schedule.js
@@ -4,7 +4,7 @@ export default SirenSerializer.extend({
   // TODO: this is spoofed, and not yet gathered from payload as it's not currently provided by the API
   extractMeta(store, type /*, payload*/ ) {
     store.setMetadataFor(type, {
-      dayStartAsMinutes: 6 * 60,
+      dayStartAsMinutes: 0,
       dayEndAsMinutes: 0
     });
   }
