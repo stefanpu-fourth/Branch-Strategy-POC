f105b7bfcd77c670f2a0aac686ef28328f035b1f
Steve Sims <steve.sims@fourth.com>
2015-07-24 11:40:58 +0100

    updated to proper siren-adapter release
    fixed an issue with how the rota-schedule serialiser sets (fake) metadata
    made jscs ignore modules files

--- app/serializers/rota-schedule.js
+++ app/serializers/rota-schedule.js
@@ -2,10 +2,12 @@ import SirenSerializer from './ess';
 
 export default SirenSerializer.extend({
   // TODO: this is spoofed, and not yet gathered from payload as it's not currently provided by the API
-  extractMeta(store, type /*, payload*/ ) {
-    store.setMetadataFor(type, {
+  extractMeta(store, type, payload) {
+    payload.meta = {
       dayStartAsMinutes: 0,
       dayEndAsMinutes: 0
-    });
+    };
+
+    return this._super(...arguments);
   }
 });
