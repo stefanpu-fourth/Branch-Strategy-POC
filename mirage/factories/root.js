506606fbd4c4f930856a88a0d7a9009c51da4d49
avcengineering <Andrew.vanCampenhout@fourth.com>
2015-12-07 11:48:12 +0000

    define mirage models for all models
    factory rota-schedule and root fixes

--- mirage/factories/root.js
+++ mirage/factories/root.js
@@ -1,5 +1,6 @@
 import Mirage, { faker } from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
+  employeeId(i) { return i; },
   token: faker.random.uuid
 });
--- mirage/factories/rota-schedule.js
+++ mirage/factories/rota-schedule.js
@@ -2,20 +2,20 @@ import Mirage, { faker } from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
   type() { return 'ON'; },
-  shiftDate: faker.data.recent,
+  shiftDate: faker.date.recent,
   location: faker.address.streetName,
   department: faker.commerce.department,
   jobTitle: faker.name.jobTitle,
   isMain: faker.random.boolean,
   shiftTimes() {
     return [
-      '00:00',
-      '00:00',
-      '00:00',
-      '00:00'
+      '09:00',
+      '12:00',
+      '14:00',
+      '17:00'
     ];
   },
-  rotaStart: faker.data.recent,
+  rotaStart: faker.date.recent,
   rotaStartDayOfWeek() {
     return Math.floor(Math.random() * 7) + 1;
   }
