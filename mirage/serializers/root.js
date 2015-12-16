7537a15e35c86bba72e0e57b94ef6c071f034e32
avcengineering <Andrew.vanCampenhout@fourth.com>
2015-12-16 17:40:50 +0000

    remove root serializer (redundant)

--- mirage/serializers/root.js
+++
@@ -1,9 +0,0 @@
-import ApplicationSerializer from './application';
-
-export default ApplicationSerializer.extend({
-
-  serialize(response) {
-    return this._serializePrimaryModel(response[0]);
-  }
-
-});
