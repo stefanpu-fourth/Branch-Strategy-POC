03f716b71df1e1c1cfcf436ebbeaef636dbe5ea3
avcengineering <Andrew.vanCampenhout@fourth.com>
2016-03-31 16:57:30 +0100

    remove payslip serializer munging

--- app/serializers/payslip.js
+++
@@ -1,9 +0,0 @@
-import SirenSerializer from './ess';
-
-export default SirenSerializer.extend({
-  deriveRelationshipNameFromEntity(relationshipMeta /*, sirenRelName*/ ) {
-    const name = this._super(relationshipMeta);
-
-    return name === 'payslipElement' ? 'payslipElements' : name;
-  }
-});
