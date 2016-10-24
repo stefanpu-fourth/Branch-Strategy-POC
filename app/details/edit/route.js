c96bce2ec2c427668c04dee3563e9c80e873f6cf
Stoyan Ekupov <sekupov@Stoyans-MacBook-Pro.local>
2016-10-24 09:16:08 +0300

    Add header for details/edit page

--- app/details/edit/route.js
+++ app/details/edit/route.js
@@ -4,11 +4,19 @@ export default Ember.Route.extend({
 
   /**
     Renders `details/edit/template.hbs` into main application outlet.
+    Renders 'details/edit/header/template.hbs' in application 'nav' outlet.
 
     @method renderTemplate
     @public
   */
-  renderTemplate: function() {
-    this.render('details.edit', { into: 'application' });
+  renderTemplate() {
+    this.render('details.edit', {
+      into: 'application'
+    });
+
+    this.render('details.edit.header', {
+      into: 'application',
+      outlet: 'nav'
+    });
   }
 });
