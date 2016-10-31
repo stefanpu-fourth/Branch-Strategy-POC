515eb55cbfdaf28c4892f23c9eed4633e12c356a
Stoyan Ekupov <stoyan.ekupov@fourth.com>
2016-10-31 17:47:59 +0200

    Refator tabbed-container

--- app/details/edit/controller.js
+++ app/details/edit/controller.js
@@ -1,18 +1,23 @@
 import Ember from 'ember';
+import i18n from 'ess/i18n';
 
 export default Ember.Controller.extend({
   details: Ember.inject.controller(),
   tabSelected: Ember.computed.reads('details.selectedForEdit'),
-  nameTabSelected: Ember.computed('tabSelected', function() {
-    return this.get('tabSelected') === 'name';
-  }),
-  contactTabSelected: Ember.computed('tabSelected', function() {
-    return this.get('tabSelected') === 'contact';
-  }),
-
-  actions: {
-    selectTab(tabName) {
-      this.set('tabSelected', tabName);
-    }
-  }
+  tabContainerConfig: Ember.computed('tabSelected', function() {
+    return [
+      {
+        tabName: 'name',
+        tabTitle: i18n.t('details.name'),
+        tabContent: 'name-edit-form',
+        active: this.get('tabSelected') === 'name'
+      },
+      {
+        tabName: 'contact',
+        tabTitle: i18n.t('details.contact'),
+        tabContent: 'contact-edit-form',
+        active: this.get('tabSelected') === 'contact'
+      }
+    ];
+  })
 });
