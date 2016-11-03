47059a24999618f34b6931ee4513e11e57513ff2
Stoyan Ekupov <stoyan.ekupov@fourth.com>
2016-11-03 17:29:16 +0200

    Add translations for name and contact forms

--- app/components/edit-name-form/component.js
+++ app/components/edit-name-form/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import i18n from 'ess/i18n';
+
+const titlesArr = i18n.t('details.employee.titles');
+titlesArr.unshift('');
 
 export default Ember.Component.extend({
+  titles: titlesArr
 });
