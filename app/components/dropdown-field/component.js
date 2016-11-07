9262b03211bbbd95af4ace0cefad1bce3525556e
Stoyan Ekupov <stoyan.ekupov@fourth.com>
2016-11-07 16:33:01 +0200

    Refactor

--- app/components/dropdown-field/component.js
+++ app/components/dropdown-field/component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isFocused: Ember.computed('value', function() {
+  isFocused: Ember.computed('selectedItem', function() {
     return this.get('selectedItem') ? true : false;
   }),
   actions: {
@@ -13,7 +13,6 @@ export default Ember.Component.extend({
     },
     selectItem(item) {
       this.set('selectedItem', item);
-      console.log(this.get('selectedItem'));
     }
   }
 });
--- app/components/tabbed-container/component.js
+++ app/components/tabbed-container/component.js
@@ -4,18 +4,17 @@ export default Ember.Component.extend({
   activeTab: Ember.computed(function() {
     return this.get('tabs').find(tab => tab.active);
   }),
-  activeTabComponent: Ember.computed(function() {
+  activeTabComponent: Ember.computed('activeTab', function() {
     return this.get('activeTab').component;
   }),
-  activeTabComponentModel: Ember.computed(function() {
+  activeTabComponentModel: Ember.computed('activeTab', function() {
     return this.get('activeTab').componentModel;
   }),
 
   actions: {
     setTabActive(tabName) {
       const tab = this.get('tabs').find(tab => tab.name === tabName);
-      this.set('activeTabComponent', tab.component);
-      this.set('activeTanComponentModel', tab.componentModel);
+      this.set('activeTab', tab);
 
       this.$('.tabbed-container__tab--active').removeClass('tabbed-container__tab--active');
       this.$(`#${tabName}-tab`).addClass('tabbed-container__tab--active');
