7351c16c5e8bd2dd0d7053d808d824265cc949be
Lionel Adams <lionel.adams@fourth.com>
2016-11-10 14:04:36 +0000

    Add test data filter flags to rota schedule adapter
    config change - align user name to correct user

--- app/adapters/rota-schedule.js
+++ app/adapters/rota-schedule.js
@@ -1,4 +1,5 @@
 import EssAdapter from './ess';
+import config from 'ess/config/environment';
 
 export default EssAdapter.extend({
   _buildOdataUrl: function(type, qstring) {
@@ -6,11 +7,20 @@ export default EssAdapter.extend({
   },
 
   query: function(store, type, query) {
-    var qstring = '';
-    if (query.RequestDate) {
+    const isDev = (config.environment === 'development') ? true : false;
+    let qstring = '';
+
+    // If we are in the development environment,
+    // we serve up the RedirectToUI and DefaultTestData flags
+    // so the API will serve up some dummy data
+    if (isDev) {
+      qstring = `$filter=RedirectToUI eq true and DefaultTestData eq true`;
+    } else if (query.RequestDate) {
       qstring = `$filter=RequestDate eq ${query.RequestDate} and NoPreviousWeeks eq 2 and NoFutureWeeks eq 2`;
     }
-    var url = this._buildOdataUrl(type, qstring);
+
+    const url = this._buildOdataUrl(type, qstring);
+
     return this.ajax(url);
   }
 });
