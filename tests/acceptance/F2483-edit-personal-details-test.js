c44e19958ca650ae746209c0b2f2296e8c574e1f
Stefan Uzunov <Stefan.Uzunov@fourth.com>
2017-02-06 17:35:37 +0200

    Make changes to coasts

--- tests/acceptance/F2483-edit-personal-details-test.js
+++ tests/acceptance/F2483-edit-personal-details-test.js
@@ -11,8 +11,8 @@ const sixtyLenghtString = 'SixtySymbolsLengthDoeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 const sixtyOneLenghtString = 'SixtySymbolsLengthDoeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab';
 const thirtyTwoLengthString = 'thirtyTwoLengthStringaaaaaaaaaaa';
 const thirtyThreeLengthString = 'thirtyTwoLengthStringaaaaaaaaaaab';
-const twentyLengthPhone = '00234567890123456789';
-const twentyOneLengthPhone = '002345678901234567890';
+const twentyLengthPhone = '002345127890123456789';
+const twentyOneLengthPhone = '00234567890145634567890';
 
 const nameForm = page.modal.tabsContent.editNameForm;
 const contactForm = page.modal.tabsContent.editContactForm;
