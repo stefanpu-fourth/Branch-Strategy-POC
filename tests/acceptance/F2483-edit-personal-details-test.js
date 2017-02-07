798d09235d1ed8936a52e5d7258d5e23e8bf4a20
Anton Atanasov <anton.atanasov@fourth.com>
2017-02-07 11:17:58 +0200

    small change

--- tests/acceptance/F2483-edit-personal-details-test.js
+++ tests/acceptance/F2483-edit-personal-details-test.js
@@ -11,8 +11,8 @@ const sixtyLenghtString = 'SixtySymbolsLengthDoeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 const sixtyOneLenghtString = 'SixtySymbolsLengthDoeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab';
 const thirtyTwoLengthString = 'thirtyTwoLengthStringaaaaaaaaaaa';
 const thirtyThreeLengthString = 'thirtyTwoLengthStringaaaaaaaaaaab';
-const twentyLengthPhone = '002345127890123456789';
-const badVariable = "Creating conflicts since ever";
+const twentyLengthPhone = '022345127890123456789';
+const badVariable = "Creating conflictssss since ever";
 const twentyOneLengthPhone = '02234567890145634567890';
 
 const nameForm = page.modal.tabsContent.editNameForm;
