01637928371ee5e523fdb77492776481de28f06a
Anton Atanasov <anton.atanasov@fourth.com>
2017-02-08 13:41:31 +0200

    eleventh occurence

--- app/details/route.js
+++ app/details/route.js
@@ -43,7 +43,7 @@ export default Ember.Route.extend(FindWithCache, RenderNav, ErrorNotifications, 
     return Ember.RSVP.hash({
       employment: this.findAllWithCache('mainemployment2'),
       employee: this.findWithCache('employee', this.get('appStateService.authenticatedEmployeeId')),
-      user: "some2"
+      user: "some more"
       emptyOne: {
         id: 0,
         name: 'empty one'
